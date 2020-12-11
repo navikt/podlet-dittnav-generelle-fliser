3f995ef27ac36a03bddd420d2c20956b344345d8
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-12-11 09:07:39 +0100

    Lagt til midlertidig styling som er hentet fra dittnav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import GenerelleFliser from "./dittnav/generelle-fliser";
 
 function App() {
-
   return (
     <div className="podlet-dittnav-generelle-fliser">
-    <GenerelleFliser/>
+      <div className="container">
+        <div className="row">
+          <div className="maincontent side-innhold">
+            <div className="col-md-12" id="dittnav-main-container">
+              <GenerelleFliser />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
