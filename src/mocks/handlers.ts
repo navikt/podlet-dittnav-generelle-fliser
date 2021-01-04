b695769594fcf9ec83c1f9caa61f4651f6147f8a
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-01-04 07:12:41 +0100

    Fikser urls for msw så man ikke blir blokkert på CORS i dev

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,4 @@
 import get from "./msw-utils";
 import oppfolging from "./oppfolging.json";
 
-export const handlers = [
-  get("https://www.dev.nav.no/person/dittnav-api/oppfolging", oppfolging),
-];
+export const handlers = [get("https://www.nav.no/person/dittnav-api/oppfolging", oppfolging)];
